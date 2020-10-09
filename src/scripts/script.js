b488a167c9e6f8efc99c8841ef385a309fc01c58
Alexander Vasiliev <selado@ya.ru>
2020-10-09 16:28:07 +0300

    add src

---
+++ src/scripts/script.js
@@ -0,0 +1,26 @@
+import '../pages/index.css';
+
+// import Swiper, { Pagination } from 'swiper';
+// Swiper.use([Pagination]);
+
+'use strict';
+
+// (function() {
+//
+//   new Swiper('.reviews__swiper-container', {
+//     init: true,
+//     loop: true,
+//     grabCursor: true,
+//     wrapperClass: 'reviews__swiper-wrapper',
+//     slideClass: 'reviews__card',
+//
+//     pagination: {
+//       el: '.reviews__pagination',
+//       type: 'bullets',
+//       clickable: true,
+//       bulletClass: 'reviews__pagination-item',
+//       bulletActiveClass: 'reviews__pagination-item_active',
+//     },
+//   })
+//
+// })();
